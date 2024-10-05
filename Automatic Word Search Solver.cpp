02e8a90c5a98c6abc8cd62e3f90b26f8e0160029
Caden Do <ctdo@umich.edu>
2024-10-05 02:07:08 -0400

    Updated searching algorithm from the poorly written duplicated search algorithm when code was initally written during time at WCC. Added directions 4 to 7 (Horizonal and vertical searches in both respective directions)
    Cleaned up search section, made cleaner with OR statements and spreaded coded out a little bit to make more readable. Don't know if I want to split up the int DiagRow and DiagCol or just leave it alone

--- Automatic Word Search Solver.cpp
+++ Automatic Word Search Solver.cpp
@@ -61,72 +61,45 @@ bool WordSearch(const char table[ArraySize][ArraySize], const string& word, int&
     // +1 is down/right, -1 is up/left
 
     for (int x = 0; x < ArraySize; x++) {
-        
-        // Horizontal Search (2 directions)
-        for (int y = 0; y <= ArraySize; y++) {
-            for (int HorzDirection = -1; HorzDirection <= 1; HorzDirection += 2) {
-                bool WordFoundNormHoriz = true;
-                for (int i = 0; i < word.size(); i++) {
-                    int HorzCol = y + i * HorzDirection; // calculating what column it is on
-                    if (HorzCol < 0 || HorzCol >= ArraySize || table[x][HorzCol] != word[i]) { // Keeping search in bound and see if match
-                        WordFoundNormHoriz = false;
-                        break;
-                    }
-                }
-                if (WordFoundNormHoriz) {
-                    string HorzDirectionStr = (HorzDirection == 1) ? "left" : "right";
-                    cout << word << " was found horizontally " << HorzDirectionStr << " at row " << x << " and column " << y << endl;
-                    FoundWords++;
-                    return true;
-                }
-            }
-        }
-
-        // Vertical Search (2 directions)
-        for (int y = 0; y < ArraySize; y++) {
-            for (int VertDirection = -1; VertDirection <= 1; VertDirection += 2) {
-                bool WordFoundVert = true;
-                for (int i = 0; i < word.size(); i++) {
-                    int VertRow = x + i * VertDirection; // calculate what row it is on
-                    if (VertRow < 0 || VertRow >= ArraySize || table[VertRow][y] != word[i]) { // Keeping search in bound and see if match
-                        WordFoundVert = false;
-                        break;
-                    }
-                }
-                if (WordFoundVert) {
-                    string VertDirectionStr = (VertDirection == 1) ? "right" : "left";
-                    cout << word << " was found vertically " << VertDirectionStr << " at row " << x << " and column " << y << endl;
-                    FoundWords++;
-                    return true;
-                }
-            }
-        }
 
         // Diagonal Search (4 directions)
         for (int y = 0; y < ArraySize; y++) {
-            for (int DiagDirection = 0; DiagDirection < 4; DiagDirection++) {
-                // 0 is top left to bottom right
-                // 1 is top right to bottom left 
-                // 2 is bottom right to top left
-                // 3 is bottom left to top right 
-                bool WordFoundDiag = true;
-                for (int i = 0; i < word.size(); i++) {
-                    int DiagRow = x + i * (DiagDirection == 0 ? 1 : (DiagDirection == 1 ? 1 : (DiagDirection == 2 ? -1 : -1)));  // Calculating movement for row
-                    int DiagCol = y + i * (DiagDirection == 0 ? 1 : (DiagDirection == 1 ? -1 : (DiagDirection == 2 ? -1 : 1))); // Calculating movement for col
-                    if (DiagCol < 0 || DiagRow < 0 || DiagCol >= ArraySize || DiagRow >= ArraySize || table[DiagRow][DiagCol] != word[i]) { // Keeping search in bound and see if match
-                        WordFoundDiag = false;
-                        break;
-                    }
-                }
-                if (WordFoundDiag) {
-                    string DiagDirectionStr = (DiagDirection == 0 ? "down-right" : (DiagDirection == 1 ? "down-left" : (DiagDirection == 2 ? "up-left" : "up-right")));
-                    cout << word << " was found diagonally " << DiagDirectionStr << " at row " << x << " and column " << y << endl;
-                    FoundWords++;
-                    return true;
-                    break;
-                }
+    for (int DiagDirection = 0; DiagDirection < 8; DiagDirection++) {
+        // 0 is top left to bottom right (diagonal down-right)
+        // 1 is top right to bottom left (diagonal down-left)
+        // 2 is bottom right to top left (diagonal up-left)
+        // 3 is bottom left to top right (diagonal up-right)
+        // 4 is horizontal left to right
+        // 5 is horizontal right to left
+        // 6 is vertical top to bottom
+        // 7 is vertical bottom to top
+        
+        bool WordFoundDiag = true;
+
+        for (int i = 0; i < word.size(); i++) {
+            int DiagRow = x + i * ((DiagDirection == 0 || DiagDirection == 1 || DiagDirection == 6) ? 1 : (DiagDirection == 2 || DiagDirection == 3 || DiagDirection == 7) ? -1 : 0);
+            int DiagCol = y + i * ((DiagDirection == 0 || DiagDirection == 3 || DiagDirection == 4) ? 1 :  (DiagDirection == 1 || DiagDirection == 2 || DiagDirection == 5) ? -1 : 0);
+            if (DiagCol < 0 || DiagRow < 0 || DiagCol >= ArraySize || DiagRow >= ArraySize || table[DiagRow][DiagCol] != word[i]) {
+                WordFoundDiag = false;
+                break;
             }
         }
+        if (WordFoundDiag) {
+            string DiagDirectionStr = (DiagDirection == 0 ? "down-right" : 
+                                       DiagDirection == 1 ? "down-left" : 
+                                       DiagDirection == 2 ? "up-left" : 
+                                       DiagDirection == 3 ? "up-right" : 
+                                       DiagDirection == 4 ? "right" : 
+                                       DiagDirection == 5 ? "left" : 
+                                       DiagDirection == 6 ? "down" :
+                                       DiagDirection == 7 ? "up" : "unknown");
+
+            cout << word << " was found " << DiagDirectionStr << " at row " << x << " and column " << y << endl;
+            FoundWords++;
+            return true;
+        }
+    }
+}
     }
     return false;
 }
